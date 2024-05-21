e97bb7605dcb08ce00a8b3e841c238018bca114a
namcao <cunnconn01@gmail.com>
2024-05-21 13:11:48 +0700

    cxc

--- src/com/caro/Main.java
+++ src/com/caro/Main.java
@@ -2,9 +2,10 @@ package com.caro;
 
 public class Main {
 
-    public static  JFrameMain jFrameMain;
-    public static void main(String[] args){
-       jFrameMain= new JFrameMain();
-       jFrameMain.CreateAndShow();
+    public static JFrameMain jFrameMain;
+
+    public static void main(String[] args) {
+        jFrameMain = new JFrameMain();
+        jFrameMain.CreateAndShow();
     }
 }
